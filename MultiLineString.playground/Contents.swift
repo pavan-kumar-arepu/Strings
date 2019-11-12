d8bcdc059fd26d690f07be3120a657d0edb5c949
Pavankumar arepu <iosdeveloper.ipa@gmail.com>
2019-11-13 01:15:41 +0530

    Stings
    Some Examples on MultiLine Strings

---
+++ MultiLineString.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "MultiLine String"
+
+let quotation_multilineString = """
+The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
+
+
+let singleLineString = "These are the same."
+let multilineString = """
+These are the same.
+"""
+
+
+print("single line\(singleLineString)")
+
+print("quotation line\(quotation_multilineString)")
+
+
